0015264de55c9b75b78e303df3a33a5fc9bfe9ed
Shubham Sahu <shubham.sahu.techie@gmail.com>
2026-07-06 22:27:40 +0530

    program to display all perfect number from 1 to 1000

--- src/Factors.java
+++ src/Factors.java
@@ -9,27 +9,37 @@ public class Factors {
 
 //        System.out.println(new Factors().c);
 
-        Scanner s = new Scanner(System.in);
+//        Scanner s = new Scanner(System.in);
+//
+//        int a = s.nextInt();
+        {
 
-        int a = s.nextInt();
-        int sum = 0;
+            for (int a = 1; a < 1000; a++) {
+                int sum = 0;
+
+
 //        for (int i = 1; i <= a; i++) {
-        for (int i = 1; i < a; i++) {
-            if (a % i == 0) {
-                sum += i;
-                System.out.println(" factors is " + i);
+                for (int i = 1; i < a; i++) {
+                    if (a % i == 0) {
+                        sum += i;
+//                        System.out.println(" factors is " + i);
 
 
+                    }
+
+                }
+//                System.out.println("sum to a  foactor" + sum);
+
+                if (sum == a) {
+
+
+                    System.out.println(a);
+//            System.out.println("perfect number");
+//        }else {
+//            System.out.println("nit a perfect number");
+                }
             }
-
         }
-        System.out.println("sum to a  foactor" + sum);
 
-        if (sum == a ){
-
-            System.out.println("perfect number");
-        }else {
-            System.out.println("nit a perfect number");
-        }
     }
 }
