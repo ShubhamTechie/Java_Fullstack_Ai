c7312a5f6dde7a53a554e599f031715710259f65
Shubham Sahu <shubham.sahu.techie@gmail.com>
2026-07-06 20:24:38 +0530

    sum of a factors and  perfect number

--- src/Factors.java
+++ src/Factors.java
@@ -2,25 +2,34 @@ import java.util.Scanner;
 
 public class Factors {
 
-    int c = 1;
+//    int c = 1;
 
     public static void main(String[] args) {
 
 
-        System.out.println(new Factors().c);
+//        System.out.println(new Factors().c);
 
         Scanner s = new Scanner(System.in);
 
         int a = s.nextInt();
         int sum = 0;
-        for (int i = 1; i <= a; i++) {
+//        for (int i = 1; i <= a; i++) {
+        for (int i = 1; i < a; i++) {
             if (a % i == 0) {
                 sum += i;
                 System.out.println(" factors is " + i);
+
+
             }
+
         }
         System.out.println("sum to a  foactor" + sum);
-        System.out.println("sum to a  foactor" + sum);
 
+        if (sum == a ){
+
+            System.out.println("perfect number");
+        }else {
+            System.out.println("nit a perfect number");
+        }
     }
 }
